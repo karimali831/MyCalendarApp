d93a02ce9287dde4d9174af0538170d2f9b1920a
Karim Ali <karimali831@googlemail.com>
2020-09-06 12:53:47 +0100

    Initial commit for MVC calendar project

---
+++ MyFinances/Model/Finance.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace MyFinances.Model
+{
+    public class Finance
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class FinanceMap : EntityTypeConfiguration<Finance>
+    {
+        public FinanceMap()
+        {
+            // Primary Key
+            this.HasKey(t => t.Id);
+
+            // Properties
+            // Table & Column Mappings
+            this.ToTable("Finances");
+            this.Property(t => t.Id).HasColumnName("Id");
+
+            // Relationships
+
+        }
+    }
+}
---
+++ MyFinances/Repository/Finance.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using DFM.Utils;
+using MyFinances.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFinances.Repository
+{
+    public interface IFinanceRepository
+    {
+        Task<IEnumerable<Finance>> GetAllAsync();
+    }
+
+    public class FinanceRepository : IFinanceRepository
+    {
+        private readonly Func<IDbConnection> dbConnectionFactory;
+        private static readonly string TABLE = "Finances";
+        private static readonly string[] FIELDS = typeof(Finance).DapperFields();
+
+        public FinanceRepository(Func<IDbConnection> dbConnectionFactory)
+        {
+            this.dbConnectionFactory = dbConnectionFactory ?? throw new ArgumentNullException(nameof(dbConnectionFactory));
+        }
+
+        public async Task<IEnumerable<Finance>> GetAllAsync()
+        {
+            using (var sql = dbConnectionFactory())
+            {
+                return (await sql.QueryAsync<Finance>($"{DapperHelper.SELECT(TABLE, FIELDS)}")).ToArray();
+            }
+        }
+    }
+}
